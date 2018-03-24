b0bae201fb1cee1d77ff1da50e5ae1b9eed10b81
shaneelu <sl2363@cornell.edu>
2018-03-24 16:08:05 -0400

    Update a4988.py

--- a4988.py
+++ a4988.py
@@ -5,11 +5,16 @@ GPIO.setup(top_scan_pin, GPIO.INPUT)
 GPIO.setup(bottom_scan_pin, GPIO.INPUT)
 
 def stop():
-    print('stopping the scanner when it reaches the bottom')
-    GPIO.output(enablePin1, GPIO.HIGH)
+    print('stopping the scanner when it reaches the top or bottom')
+    GPIO.output(enablePin1, GPIO.HIGH) #disable driver
 
-GPIO.add_event_detect(bottom_scan_pin, GPIO.RISING, callback=stop)
-GPIO.add_event_detect(top_scan_pin, GPIO.RISING, callback=stop)
+GPIO.add_event_detect(bottom_scan_pin, GPIO.RISING, callback=stop, bouncetime=2000)
+GPIO.add_event_detect(top_scan_pin, GPIO.RISING, callback=stop, bouncetime=2000)
 
 def reset():
-
+    moveScannerUp(10000)
+    moveScannerDown(200) #release from pressing the switch
+    moveVerticalUp(10000)
+    moveVerticalDown(200) #release from pressing the switch
+    moveHorizontalLeft(10000)
+    moveHorizontalRight(200) #release from pressing the switch
